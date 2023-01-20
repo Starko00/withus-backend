4175c9c7cf9dc1491f4ee0fc0da9fcf699c36ef6
Starko00 <stefanvukmanovic77@gmail.com>
2023-01-20 16:06:18 +0100

    Server history

--- app.js
+++ app.js
@@ -2,18 +2,39 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const app = express();
-const landingRouter = require('./routes/landingRouter')
+const history = require('connect-history-api-fallback');
+const landingRouter = require("./routes/landingRouter");
 app.use((req, res, next) => {
   req.requestTime = new Date().toString();
   console.log(req.requestTime);
   next();
 });
 
-app.use(morgan("dev")); 
+app.use(history({
+	rewrites: [
+		{ from: /\/*/, to: (context) => {
+			console.log(context.parsedUrl.pathname);
+			if(context.parsedUrl.pathname.indexOf('/asset-manifest.json') == 0
+			   || context.parsedUrl.pathname.indexOf('/img') == 0
+			   || context.parsedUrl.pathname.indexOf('/static') == 0
+			   || context.parsedUrl.pathname.indexOf('/api') == 0
+			   || context.parsedUrl.pathname.indexOf('/manifest.json') == 0
+			   || context.parsedUrl.pathname.indexOf('/robots.txt') == 0){
+				return context.parsedUrl.pathname;
+			}
+			else return '/index.html'}
+		},
+	]
+}));
+
+
+
+
+app.use(morgan("dev"));
 app.use(express.json()); //Enables req reading
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/',express.static('../withus-build'))
-app.use("/withus/api/v1/landing", landingRouter)
+app.use("/", express.static("../withus-build"));
+app.use("/withus/api/v1/landing", landingRouter);
 
 module.exports = app;
