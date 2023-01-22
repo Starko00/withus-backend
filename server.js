55c29b90cc501538fe9907d4406e33fdfb5ad9f6
Starko00 <stefanvukmanovic77@gmail.com>
2023-01-22 22:11:09 +0100

    Cors fix

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const app = require('./app')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const cors = require("cors");
 
+dotenv.config({ path: "./.env" }); // Config of ENV
+const port = process.env.PORT || 3000; //ENV PORT READING
 
-dotenv.config({path:'./.env'}) // Config of ENV
-const port = process.env.PORT || 3000 //ENV PORT READING
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then(() => {
+  console.log("DB connected");
+});
 
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+    methods: "GET,PUT,DELETE,POST,PATCH",
+  })
+);
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
-mongoose.connect(DB).then(()=>{console.log("DB connected")})
-
-app.use(cors())
-
-app.listen(port,()=>{
-    console.log(`Server is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
