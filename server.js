5447ebd11dc36b2808450bd8405c25482b7e447a
Starko00 <stefanvukmanovic77@gmail.com>
2023-01-22 21:59:26 +0100

    Cors

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const port = process.env.PORT || 3000 //ENV PORT READING
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 mongoose.connect(DB).then(()=>{console.log("DB connected")})
 
+app.use(cors({
+    origin:"*"
+}))
+
 app.listen(port,()=>{
     console.log(`Server is running on port: ${port}`)
 })
